6da3f1996b40fdb9ba6f51a220ba17e8600087f4
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2013-10-10 18:23:48 +0300

    Renamed 'TypeOfRelation' to 'RelationType' in RelationDao.java.

--- src/csri/poeticon/praxicon/db/dao/RelationDao.java
+++ src/csri/poeticon/praxicon/db/dao/RelationDao.java
@@ -7,7 +7,7 @@ package csri.poeticon.praxicon.db.dao;
 
 import csri.poeticon.praxicon.db.entities.Concept;
 import csri.poeticon.praxicon.db.entities.Relation;
-import csri.poeticon.praxicon.db.entities.TypeOfRelation;
+import csri.poeticon.praxicon.db.entities.RelationType;
 import csri.poeticon.praxicon.db.entities.UnionOfIntersections;
 import java.util.List;
 
@@ -21,13 +21,13 @@ public interface RelationDao extends Dao<Long, Relation> {
     List<UnionOfIntersections> getObjRelations(Concept c);
     List<Concept> relatedConcepts(Concept c);
     List<Relation> allRelationsOf(Concept c);
-    List<Relation> allRelationsOfByRelationType(Concept c, TypeOfRelation type);
-    List<Relation> allRelationsOfByRelationName(Concept concept, TypeOfRelation.RELATION_NAME name);
+    List<Relation> allRelationsOfByRelationType(Concept c, RelationType type);
+    List<Relation> allRelationsOfByRelationName(Concept concept, RelationType.RELATION_NAME name);
     boolean areRelated(Concept c1, Concept c2);
     public List<Relation> getSubConcepts(Concept c);
     List<Concept> getObjectsOfARelation(String nameOfTheRelation);
     List<Relation> findByType(String nameOfTheRelation);
     public List<Concept> getRelatedConcepts(Concept c);
-    List<Relation> findRelationsByConceptTypeOfRelation(Concept concept, TypeOfRelation type);
+    List<Relation> findRelationsByConceptTypeOfRelation(Concept concept, RelationType type);
     boolean isPartOfInherentUnion (Relation r);
 }
