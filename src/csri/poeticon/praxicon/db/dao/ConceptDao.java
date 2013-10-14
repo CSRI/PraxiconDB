6e1da2d5826e590cd1fe212b106d53e7e3dec745
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2013-10-14 21:53:20 +0300

    Reordered method declarations according to naming.

--- src/csri/poeticon/praxicon/db/dao/ConceptDao.java
+++ src/csri/poeticon/praxicon/db/dao/ConceptDao.java
@@ -8,7 +8,7 @@ package csri.poeticon.praxicon.db.dao;
 import csri.poeticon.praxicon.db.entities.Concept;
 import csri.poeticon.praxicon.db.entities.Concept.Status;
 import csri.poeticon.praxicon.db.entities.RelationType;
-import csri.poeticon.praxicon.db.entities.UnionOfIntersections;
+//import csri.poeticon.praxicon.db.entities.UnionOfIntersections;  // Obsolete
 import csri.poeticon.praxicon.db.entities.VisualRepresentation;
 import java.util.List;
 
@@ -21,31 +21,30 @@ public interface ConceptDao extends Dao<Long, Concept>{
     @Override
     List<Concept> findAll();
     List<Concept> findAllByLanguageRepresentation(String queryString);
+    List<Concept> findByLanguageRepresentation(String queryString);
     List<Concept> findAllByLanguageRepresentationStarting(String queryString);
     List<Concept> findAllByName(String name);
+    public List<Concept> findAllByNameStarting(String name);
     List<Concept> findByName(String name);
     List<Concept> findByStatus(Status status);
     VisualRepresentation getPrototypeRepresentation(Concept concept);
     Concept getConceptWithNameOrID(String v);
     Concept updatedConcept(Concept newCon);
     void update(Concept newCon);
-    boolean areRelated(Concept conA, String relation, Concept conB);
     void update(Concept oldCon, Concept newCon);
     Concept simpleUpdate(Concept oldCon, Concept newCon);
-    UnionOfIntersections getRelationUnion(Concept conA, Concept conB);
-    List<Concept> findByLanguageRepresentation(String queryString);
-    public List<Concept> findAllByNameStarting(String name);    
+    boolean areRelated(Concept conA, String relation, Concept conB);
+    // getRelationUnion(Concept conA, Concept conB);  // May be obsolete
+    
     List<Concept> getChildrenOf(Concept c);
     List<Concept> getParentsOf(Concept c);
+    public List<Concept> getAllAncestors(Concept c);
+    public List<Concept> getAllOffsprings(Concept c);
     List<Concept> getClassesOfInstance(Concept c);
     List<Concept> getInstancesOf(Concept c);
     public List<Concept> getBasicLevel(Concept c);
     public List<Concept> getBasicLevelOfAnEntityConcept(Concept con);
     public List<Concept> getBasicLevelOfAnAbstractConcept(Concept c);
-    public List<Concept> getAllAncestors(Concept c);
-    public List<Concept> getAllOffsprings(Concept c);
-
-    public void clearManager();
-
     public List<Concept> getConceptsRelatedWithByRelationType(Concept c, RelationType rtype);
+    public void clearManager();
 }
