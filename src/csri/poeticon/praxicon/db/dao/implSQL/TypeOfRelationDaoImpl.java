47f70c0bcfc35098cdf0d645a5f90e45f7b0f2f3
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2013-10-17 19:42:10 +0300

    TypeOfRelation-related changes in TypeOfRelationDaoImpl.java.

--- src/csri/poeticon/praxicon/db/dao/implSQL/TypeOfRelationDaoImpl.java
+++ src/csri/poeticon/praxicon/db/dao/implSQL/TypeOfRelationDaoImpl.java
@@ -5,22 +5,22 @@
 
 package csri.poeticon.praxicon.db.dao.implSQL;
 
-import csri.poeticon.praxicon.db.dao.TypeOfRelationDao;
-import csri.poeticon.praxicon.db.entities.TypeOfRelation;
+import csri.poeticon.praxicon.db.dao.RelationTypeDao;
+import csri.poeticon.praxicon.db.entities.RelationType;
 import javax.persistence.Query;
 
 /**
  *
  * @author Erevodifwntas
  */
-public class TypeOfRelationDaoImpl extends JpaDao<Long, TypeOfRelation> implements TypeOfRelationDao{
+public class TypeOfRelationDaoImpl extends JpaDao<Long, RelationType> implements RelationTypeDao{
     /**
-     * Creates q query to search for a TypeOfRelation using forward and backward name
-     * @param entity the TypeOfRelation to be searched
-     * @return a query to search for the TypeOfRelation
+     * Creates q query to search for a RelationType using forward and backward name
+     * @param entity the RelationType to be searched
+     * @return a query to search for the RelationType
      */
     @Override
-    public Query getEntityQuery(TypeOfRelation entity)
+    public Query getEntityQuery(RelationType entity)
     {
         Query q = getEntityManager().createQuery("SELECT e FROM TypeOfRelation e " +
                 "where e.forwardName = ?1 and e.backwardName = ?2"
