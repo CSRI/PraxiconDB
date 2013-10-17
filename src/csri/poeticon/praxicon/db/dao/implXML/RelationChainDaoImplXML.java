1686416eb446a90f95161547c99de6b8254b3976
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2013-10-17 22:51:51 +0300

    Temporarily disabled method in RelationChainDaoImplXML.java.

--- src/csri/poeticon/praxicon/db/dao/implXML/RelationChainDaoImplXML.java
+++ src/csri/poeticon/praxicon/db/dao/implXML/RelationChainDaoImplXML.java
@@ -22,36 +22,43 @@ import javax.persistence.Query;
  */
 public class RelationChainDaoImplXML extends JpaDao<Long, RelationChain> implements RelationChainDao{
 
+
     @Override
-    public List<RelationChain> getRelationChainsContainingConcept(Concept c)
-    {
-        List<RelationChain> res = new ArrayList<RelationChain>();
-        
-        Enumeration en = Constants.globalConcepts.elements();
-        while(en.hasMoreElements())
-        {
-            Concept con = (Concept)en.nextElement();
-            for (int i =0; i < con.getRelations().size(); i++)
-            {
-                for (int j = 0; j < con.getRelations().get(i).getIntersections().size(); j++)
-                {
-                    for (int k = 0; k < con.getRelations().get(i).getIntersections().get(j).getRelations().size(); k++)
-                    {
-                        List<Relation> rc = con.getRelations().get(i).getIntersections().get(j).getRelations().get(k).getActualRelations();
-                        for (int l = 0; l < rc.size(); l++)
-                        {
-                            if (rc.get(l).getSubject().getName().equalsIgnoreCase(c.getName()) || rc.get(l).getObject().getName().equalsIgnoreCase(c.getName()))
-                            {
-                                res.add(con.getRelations().get(i).getIntersections().get(j).getRelations().get(k));
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return res;
+    public List<RelationChain> getRelationChainsContainingConcept(Concept c) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
+
+
+//    @Override
+//    public List<RelationChain> getRelationChainsContainingConcept(Concept c)
+//    {
+//        List<RelationChain> res = new ArrayList<RelationChain>();
+//
+//        Enumeration en = Constants.globalConcepts.elements();
+//        while(en.hasMoreElements())
+//        {
+//            Concept con = (Concept)en.nextElement();
+//            for (int i =0; i < con.getRelations().size(); i++)
+//            {
+//                for (int j = 0; j < con.getRelations().get(i).getIntersections().size(); j++)
+//                {
+//                    for (int k = 0; k < con.getRelations().get(i).getIntersections().get(j).getRelations().size(); k++)
+//                    {
+//                        List<Relation> rc = con.getRelations().get(i).getIntersections().get(j).getRelations().get(k).getActualRelations();
+//                        for (int l = 0; l < rc.size(); l++)
+//                        {
+//                            if (rc.get(l).getSubject().getName().equalsIgnoreCase(c.getName()) || rc.get(l).getObject().getName().equalsIgnoreCase(c.getName()))
+//                            {
+//                                res.add(con.getRelations().get(i).getIntersections().get(j).getRelations().get(k));
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return res;
+//    }
 
     @Override
     public Query getEntityQuery(RelationChain entity)
