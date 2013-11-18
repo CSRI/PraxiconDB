11f3ff1423ba132e6beaede1bfe10c83d857b1bf
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2013-11-18 22:17:11 +0200

    Modified testJPA.java.

--- src/csri/poeticon/praxicon/testJPA.java
+++ src/csri/poeticon/praxicon/testJPA.java
@@ -60,83 +60,81 @@ public class testJPA {
             con.getLanguageRepresentations().add(lr.get(i));
         }
 
+
+        //How to add a relation
+        //1.) Create the IntersectionOfRelations
+        IntersectionOfRelationChains inter = new IntersectionOfRelationChains();
+        //2.) Create the Relation Chain of the relation
+        RelationChain rChain = new RelationChain();
+        //3.) Create the relation for this relation chain
+        Relation r1 = new Relation();
+        //4.a.) Create the type of the relation
+        RelationType rType = new RelationType();
+        rType.setForwardName(RelationType.relation_name.PART_OF);
+        rType.setBackwardName(RelationType.relation_name.HAS_PART);
+        //4.b) Set the type to the relation
+        r1.setTypeSimple(rType);
+        //r1.setType(rType);
+        //4.c) set the object and the subject of the relation
+        //(if the concepts do not exist you should create them -just set their name
+        //the name should be unique -it works like their ID)
+        Concept objectTest = new Concept();
+        objectTest.setName("objectTestConcept");
+        r1.setObject(objectTest);
+        r1.setSubject(con);
+        //5.) Add the relation to the chain
+        //the second argument is the order of the relation in the chain
+        rChain.addRelation(r1, 0);
+        //6.) add the chain to the Intersection
+        inter.getRelations().add(rChain);
+        //7.) add the intersection to the Union
+        //union.getIntersections().add(inter);
+        //8.) add the union to the Concept
+        //con.addRelation(union);
+
+        //How to: VR
+        VisualRepresentation vr =new VisualRepresentation();
+        vr.setRepresentation("this is a test");
+        vr.setMediaType("image");
+        //sub.addVR(vr);
+
+        //Adding a relation
+        //1rst step: create the TypeOfRelation (or use an existing 1)
+        RelationType type = new RelationType();
+        type.setForwardName(RelationType.relation_name.PART_OF);
+        type.setBackwardName(RelationType.relation_name.PARTIAL_INSTANCE_OF);
+
+        // 2nd step: create the relations (always there should be a type and an
+        // Object for the relation)
+        Relation rel = new Relation();
+        rel.setType(type);
+        rel.setObject(con);
+
+        // here is an example for another relation
+        Relation rel2 = new Relation();
+        rel2.setType(type);
+        rel2.setObject(con);
+
+
+        //3rd step: create the relation chain
+        RelationChain rc = new RelationChain();
+        //there should be in the correct order (the order counter must start from 0)
+        rc.addRelation(rel, 0);
+        rc.addRelation(rel2, 1);
+
+        // 4th step: create the intersection
+        inter.getRelations().add(rc);
+
+
+        // 6th step: create the Concept_UnionRelation
+        // here you should determine if the union is describing the basic use of
+        // the object or how important is this use (union) for the entity)
+        //adding the entity
         ConceptDao conceptDaoDao = new ConceptDaoImpl();
         conceptDaoDao.merge(con);
 
-//
-//        //How to add a relation
-//        //1.) Create a UnionOfIntersections
-//        // UnionOfIntersections union = new UnionOfIntersections();
-//        //2.) Create the IntersectionOfRelations
-//        IntersectionOfRelationChains inter = new IntersectionOfRelationChains();
-//        //3.) Create the Relation Chain of the relation
-//        RelationChain rChain = new RelationChain();
-//        //4.) Create the relation for this relation chain
-//        Relation r1 = new Relation();
-//            //4.a.) Create the type of the relation
-//        RelationType rType = new RelationType();
-//        rType.setForwardName(RelationType.relation_name.PART_OF);
-//        rType.setBackwardName(RelationType.relation_name.HAS_PART);
-//            //4.b) Set the type to the relation
-//        r1.setTypeSimple(rType);
-//        //r1.setType(rType);
-//            //4.c) set the object and the subject of the relation
-//            //(if the concepts do not exist you should create them -just set their name
-//            //the name should be unique -it works like their ID)
-//        Concept objectTest = new Concept();
-//        objectTest.setName("objectTestConcept");
-//        r1.setObject(objectTest);
-//        r1.setSubject(con);
-//        //5.) Add the relation to the chain
-//        //the second argument is the order of the relation in the chain
-//        rChain.addRelation(r1, 0);
-//        //6.) add the chain to the Intersection
-//        inter.getRelations().add(rChain);
-//        //7.) add the intersection to the Union
-//        //union.getIntersections().add(inter);
-//        //8.) add the union to the Concept
-//        //con.addRelation(union);
-//
-//        //How to: VR
-//        VisualRepresentation vr =new VisualRepresentation();
-//        vr.setRepresentation("this is a test");
-//        vr.setMediaType("image");
-//        //sub.addVR(vr);
-//
-//        //Adding a relation
-//        //1rst step: create the TypeOfRelation (or use an existing 1)
-//        RelationType type = new RelationType();
-//        type.setForwardName("usedFor");
-//        type.setBackwardName("uses");
-//
-//        // 2nd step: create the relations (always there should be a type and an
-//        // Object for the relation)
-//        Relation rel = new Relation();
-//        rel.setType(type);
-//        rel.setObject(sub);
-//
-//        // here is an example for another relation
-//        Relation rel2 = new Relation();
-//        rel2.setType(type);
-//        rel2.setObject(sub);
-//
-//
-//        //3rd step: create the relation chain
-//        RelationChain rc = new RelationChain();
-//        //there should be in the correct order (the order counter must start from 0)
-//        rc.addRelation(rel, 0);
-//        rc.addRelation(rel2, 1);
-//
-//        // 4th step: create the intersection
-//        inter.getRelations().add(rc);
-//
-//
-//        // 6th step: create the Concept_UnionRelation
-//        // here you should determine if the union is describing the basic use of
-//        // the object or how important is this use (union) for the entity)
-//        //adding the entity
 //        ConceptDao conceptDaoDao = new ConceptDaoImpl();
-//        conceptDaoDao.merge(sub);
+//        conceptDaoDao.merge(con);
     }
 
     public void persist(Object object)
