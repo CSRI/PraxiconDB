e703aed6faebcee1cbc256782efc136aa6cb87ca
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2013-11-19 15:03:12 +0200

    Renamed IntesectionOfRelationsDao.java to IntersectionOfRelationChainsDao.java.

---
+++ src/csri/poeticon/praxicon/db/dao/IntersectionOfRelationChainsDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package csri.poeticon.praxicon.db.dao;
+
+import csri.poeticon.praxicon.db.entities.IntersectionOfRelationChains;
+
+/**
+ *
+ * @author Dimitris Mavroeidis
+ */
+public interface IntersectionOfRelationChainsDao extends Dao<Long, IntersectionOfRelationChains>
+{
+
+}
