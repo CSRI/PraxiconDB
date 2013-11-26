6df89cd57ab0b2eff42861bc3ba97654519fda8b
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2013-11-26 14:59:41 +0200

    Implementation of the first validation rule.

---
+++ src/csri/poeticon/praxicon/db/entities/validators/ConceptValidator.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package csri.poeticon.praxicon.db.entities.validators;
+
+import java.util.List;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+//import csri.poeticon.praxicon.db.entities.Concept.ConstantConcepts;
+import csri.poeticon.praxicon.db.entities.Concept;
+import csri.poeticon.praxicon.db.entities.Concept.status;
+import csri.poeticon.praxicon.db.entities.Concept.unique_instance;
+
+
+/**
+ *
+ * @author dmavroeidis
+ */
+
+public class ConceptValidator implements ConstraintValidator<ConstantConcepts, Concept>
+{
+
+    private boolean isValid;    
+
+    @Override
+    public void initialize(ConstantConcepts constraintAnnotation) {
+        this.isValid = constraintAnnotation.value();
+    }
+
+    @Override
+    public boolean isValid(Concept value, ConstraintValidatorContext context)
+    {
+        if ((value.getStatus() == status.CONSTANT) && (value.getUniqueInstance() == unique_instance.YES))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
