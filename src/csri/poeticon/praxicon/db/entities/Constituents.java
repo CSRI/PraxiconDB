54a31d2ab38dcb290cac64698e0466b14d81792a
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2014-04-03 19:14:30 +0300

    Correct version of 'Constituents.java'.

--- src/csri/poeticon/praxicon/db/entities/Constituents.java
+++ src/csri/poeticon/praxicon/db/entities/Constituents.java
@@ -6,27 +6,16 @@
 package csri.poeticon.praxicon.db.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -47,12 +36,12 @@ public class Constituents implements Serializable {
     private Short Order;
 
     // Foreign key
-    @OneToMany(cascade=CascadeType.ALL, mappedBy = "LanguageRepresentationConstituents")
-    private List<LanguageRepresentation> ConstituentLanguageRepresentation;
+    @ManyToOne(cascade=CascadeType.ALL)
+    private LanguageRepresentation ConstituentLanguageRepresentation;
 
     // Foreign key
-    @OneToMany(cascade=CascadeType.ALL, mappedBy = "Constituents")
-    private List<LanguageRepresentation> LanguageRepresentation;
+    @ManyToOne(cascade=CascadeType.ALL)
+    private LanguageRepresentation LanguageRepresentation;
 
     public Long getId() {
         return Id;
