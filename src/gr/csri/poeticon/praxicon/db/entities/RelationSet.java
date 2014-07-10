48b231eceba130a6e7ddcbc02610a2b8d03dee72
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2014-07-10 13:12:01 +0300

    Added methods for relations retrieval in RelationSet.

--- src/gr/csri/poeticon/praxicon/db/entities/RelationSet.java
+++ src/gr/csri/poeticon/praxicon/db/entities/RelationSet.java
@@ -3,14 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gr.csri.poeticon.praxicon.db.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 
 /**
  *
@@ -18,10 +25,16 @@ import javax.persistence.Id;
  */
 @Entity
 public class RelationSet implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "RelationChainId", nullable = false)
+    @SequenceGenerator(name = "CUST_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "CUST_SEQ")
     private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "relationSet")
+    private List<RelationSet_Relation> relations;
 
     public Long getId() {
         return id;
@@ -29,6 +42,33 @@ public class RelationSet implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    /**
+     * Gets all Relations contained in this RelationSet.
+     *
+     * @return a list of relations
+     */
+    public List<Relation> getRelationsList() {
+        List<RelationSet_Relation> relationSet_RelationList = new ArrayList();
+        List<Relation> relationList = new ArrayList();
+        relationSet_RelationList = this.relations;
+        for (RelationSet_Relation relationSetRelation :
+                relationSet_RelationList) {
+            relationList.add(relationSetRelation.getRelation());
+        }
+        return relationList;
+    }
+
+    /**
+     * Gets all Relations contained in this RelationSet.
+     *
+     * @return a set of relations
+     */
+    public Set<Relation> getRelationsSet() {
+        HashSet<Relation> relationSet = new HashSet<Relation>(this.
+                getRelationsList());
+        return relationSet;
     }
 
     @Override
@@ -57,5 +97,5 @@ public class RelationSet implements Serializable {
         return "gr.csri.poeticon.praxicon.db.entities.RelationSet[ id=" + id +
                 " ]";
     }
-    
+
 }
