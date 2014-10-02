1ba849c36a14f9b19c638746b95f095a3facaaec
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2014-10-02 19:36:52 +0300

    Added some tests in "SimpleTest.java".

--- src/main/gr/csri/poeticon/praxicon/SimpleTest.java
+++ src/main/gr/csri/poeticon/praxicon/SimpleTest.java
@@ -5,9 +5,13 @@
 package gr.csri.poeticon.praxicon;
 
 import gr.csri.poeticon.praxicon.db.dao.ConceptDao;
+import gr.csri.poeticon.praxicon.db.dao.RelationArgumentDao;
+import gr.csri.poeticon.praxicon.db.dao.RelationDao;
 import gr.csri.poeticon.praxicon.db.dao.implSQL.ConceptDaoImpl;
+import gr.csri.poeticon.praxicon.db.dao.implSQL.RelationArgumentDaoImpl;
+import gr.csri.poeticon.praxicon.db.dao.implSQL.RelationDaoImpl;
 import gr.csri.poeticon.praxicon.db.entities.Concept;
-import java.util.HashSet;
+import gr.csri.poeticon.praxicon.db.entities.RelationArgument;
 import java.util.List;
 
 /**
@@ -19,41 +23,91 @@ public class SimpleTest {
 
     public static void main(String args[]) {
 
+        // Start by checking most of the methods.
+        // Concepts:
         ConceptDao cDao = new ConceptDaoImpl();
+        RelationDao rDao = new RelationDaoImpl();
 
-        //String toSearch = "String";
-        //String toSearch = "spoon%1:06:00::";
-        //String toSearch = "eating_utensil%1:06:00::";
+        // Get the number of all concepts
+        List<Concept> concepts = cDao.findAllConcepts();
+        System.out.println("\n\nNumber of all concepts: " + concepts.size());
 
-//        Query q = cDao.getEntityManager().createQuery("SELECT c FROM Concept c "
-//                        + "where c.Name = ?1");
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Concepts");
-//        EntityManager em = emf.createEntityManager();
-        //List<Concept> concepts = cDao.findConceptsByLanguageRepresentationExact("spoon");
-        List<Concept> concepts_all = cDao.findAllConcepts();
-        //Concept concept_by_name = cDao.findConceptByNameExact("spoon");
-        //List<Concept> concepts = query.getResultList();
-        //query.setParameter(1, toSearch);
-        //List<Concept> found = query.getResultList();
-        //Concept startConcept = concepts.get(0);
-        //System.out.println(concepts + " " + concepts.getSpecificityLevel());
-//        for (Concept item : concepts) {
-//            System.out.println(item.getName());
+        // Get the language representations of all concepts having "spoon" as 
+        // language representation.
+        String toSearch = "spoon";
+        List<Concept> concepts_spoon = cDao.
+                findConceptsByLanguageRepresentationExact(toSearch);
+        System.out.println("\n\nLanguage Representations of spoon: ");
+        System.out.println("---------------------------------");
+        for (Concept concept : concepts_spoon) {
+            System.out.print(concept.getLanguageRepresentationName());
+            System.out.print(" - ");
+            System.out.println(concept.getName());
+        }
+
+        // Check whether concepts "shape" and "round_shape" are related and 
+        // with what relation type.
+        System.out.println("\n\nCheck whether two concepts are related: ");
+        System.out.println("--------------------------------------- ");
+        Concept conceptRoundShape = cDao.findConceptByNameExact(
+                "round_shape%1:25:00::");
+        Concept conceptShape = cDao.findConceptByNameExact("shape%1:03:00::");
+        RelationArgumentDao raDao = new RelationArgumentDaoImpl();
+        RelationArgument relationArgumentConceptShape = raDao.
+                getRelationArgumentsByConcept(conceptShape);
+        RelationArgument relationArgumentConceptRoundShape = raDao.
+                getRelationArgumentsByConcept(conceptRoundShape);
+        boolean areRelated;
+        areRelated = rDao.areRelated(relationArgumentConceptShape,
+                relationArgumentConceptRoundShape);
+        System.out.println((areRelated) ? "shape and round_shape are related" :
+                "shape and round_shape are not related");
+        
+        
+
+// Get children concepts of the first concept that has language 
+        // representation spoon.
+//        List<Concept> children_of_spoon = cDao.getChildrenOfConcept(
+//                concepts_spoon.get(0));
+//        for (Concept concept : children_of_spoon){
+//            System.out.println(concept.getName());
 //        }
-        for (Concept item : concepts_all) {
-            System.out.println(item.getName());
-        }
-
-        HashSet<Concept> sisters = new HashSet<>();
-//
-        List<Concept> parents = cDao.getParentsOfConcept(concepts_all.get(0));
-        for (Concept parent : parents) {
-            sisters.addAll(cDao.getChildrenOfConcept(parent));
-        }
-        //sisters.remove(concepts_all.get(0));
-//
-        for (Concept con : sisters) {
-            System.out.println(con + " " + con.getSpecificityLevel());
-        }
+//System.out.println(concepts + " " + concepts.getSpecificityLevel());
+        //        for (Concept item : concepts) {
+        //            System.out.println(item.getName());
+        //        }
+        //        List<Concept> concepts_1 = cDao.findConceptsByName("spoon");
+        //        List<Concept> concepts_by_LR = cDao.findConceptsByLanguageRepresentationExact("fork");
+        //        for (Concept item : concepts_by_LR) {
+        //            System.out.println(item.getName());
+        //        }
+        //
+        //        
+        ////        for (Concept item : concepts_all) {
+        ////            System.out.println(item.getName());
+        ////        }
+        //
+        //        HashSet<Concept> sisters = new HashSet<>();
+        //        List<Concept> parents = new ArrayList<>();
+        ////        Concept concept1 = concepts_all.get(0);
+        ////
+        ////        cDao.persist(concept1);
+        //        System.out.println("ANCESTORS: " + cDao.getAllAncestors(concepts_by_LR.get(0)));
+        ////        
+        //        
+        //        //System.out.println("Concept0: " + concepts_1.get(0));
+        //
+        //        parents = cDao.getParentsOfConcept(concepts_by_LR.get(0));
+        //        for (Concept parent : parents) {
+        //            System.out.println("PARENT: " + parent);
+        //            sisters.addAll(cDao.getChildrenOfConcept(parent));
+        //        }
+        //        sisters.remove(concepts_by_LR.get(0));
+        //
+        //        System.out.println("\n\nSISTERS: \n");
+        //        
+        //        for (Concept con : sisters) {
+        //            System.out.println(con + " " + con.getSpecificityLevel());
+        //        }
     }
 }
