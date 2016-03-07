79c45d11b33300486d5d1748d0f9de536632326e
dmavroeidis <dmavroeidis@dmavroeidis-VPCF13Z1E>
2016-03-08 00:10:49 +0200

    Minor fix in MotoricRepresentationDao(Impl).

--- src/main/gr/csri/poeticon/praxicon/db/dao/MotoricRepresentationDao.java
+++ src/main/gr/csri/poeticon/praxicon/db/dao/MotoricRepresentationDao.java
@@ -14,6 +14,7 @@ import javax.persistence.Query;
 public interface MotoricRepresentationDao extends
         Dao<Long, MotoricRepresentation> {
 
+    @Override
     Query getEntityQuery(MotoricRepresentation motoricRepresentation);
 
 }
